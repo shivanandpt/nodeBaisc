46f13d2e78d8d29157a92711d6902b4498c73d22
Shivanand Patil <shivanandpatil.tech@gmail.com>
2021-07-17 12:23:38 +0530

    Set up eslint and fix lint issues
    Signed-off-by: Shivanand Patil <shivanandpatil.tech@gmail.com>

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
 const chalk = require('chalk');
-const appName = process.env.APP || "testApp";
-const morganOption = process.env.MORGAN_OPTIONS;
-const debug = require('debug')(appName);
-const port = process.env.PORT || 3000;
+const debug = require('debug')(process.env.APP);
 
+const morganOption = process.env.MORGAN_OPTIONS;
+const port = process.env.PORT || 3000;
 const app = express();
 
-debug(" booting");
+debug(' booting');
 app.use(morgan(morganOption));
 
-app.get("/", (req, res) => {
-   res.send("this is project"); 
+app.get('/', (req, res) => {
+	res.send('this is project');
 });
 
 app.listen(port, () => {
-    debug(`server running on port ${chalk.green(port)}`)
+	debug(`server running on port ${chalk.green(port)}`);
 });
